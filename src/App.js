dffdbad81d2f6164c6034dcfb337b127b88fb1c1
RR003 <mail2rahulraja@gmail.com>
2021-09-24 22:51:52 -0400

    description page changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import Header from "./components/header/header.jsx";
 import Home from "./components/home/home.jsx";
+import Description from "./components/description/description.jsx";
+import { Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <Home />
+      <BrowserRouter>
+        <Route exact path="/description" component={Description} />
+        <Route exact path="/" component={Home} />
+      </BrowserRouter>
     </div>
   );
 }
