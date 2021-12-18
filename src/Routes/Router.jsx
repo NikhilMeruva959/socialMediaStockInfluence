586a258b8be630516e8923c356ed2e6cbbdae1b8
RR003 <mail2rahulraja@gmail.com>
2021-12-17 22:43:30 -0500

    more

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,7 +1,7 @@
 import { Route, BrowserRouter } from "react-router-dom";
 import React, { Component } from "react";
 
-import Home from "../components/Home/Home";
+import Home from "../components/home/Home";
 import Trending from "../components/Trending/Trending";
 import About from "../components/About/About";
 import Header from "../components/Header/Header";
