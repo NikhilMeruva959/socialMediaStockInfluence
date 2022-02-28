abce24c13ca7b0b189990e5efc0fe400094f97b1
Nikhil Meruva <nikmeruva1@gmail.com>
2022-02-27 22:23:30 -0500

    header fix

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,10 +5,16 @@ import Container from "react-bootstrap/Container";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => (
-  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  // Using react-bootstrap for navbar
+  // expand="lg" allows for collapsing navbar at lower breakpoints
+  // bg=navbar color || variant=dark is navbar color text styling
+  // collapseOnSelect makes the Navbar collapse automatically when the user selects an item
+  <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
     <Container>
       <Navbar.Brand href="/">Stockfluence</Navbar.Brand>
+      {/* aria-controls property identifies the element (or elements) whose contents or presence are controlled by the element on which this attribute is set. */}
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link href="/getStarted">Get Started</Nav.Link>
@@ -17,7 +23,7 @@ const Header = () => (
         </Nav>
         <Nav>
           <Nav.Link href="/contact">Contact</Nav.Link>
-          <Nav.Link eventKey={2} href="/">
+          <Nav.Link href="/">
             Home
           </Nav.Link>
         </Nav>
